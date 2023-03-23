b626c7a0e0c56d7375d401b404e337053937b0db
tin6150 <tin_bofh_202206>
2023-03-22 22:32:39 -0700

    fix abricate install... some strange dependencies... using the abricate own instruction :D

--- install_abricate.sh
+++ install_abricate.sh
@@ -15,7 +15,18 @@ conda install conda=23.1.0
 conda install -c conda-forge mamba
 echo $?
 
-mamba install -c bioconda abricate
+#xx mamba install -c bioconda abricate  # fails to find perl 5.22, while container has perl 5.31.1
+
+# using instruction from abricate https://github.com/tseemann/abricate
+conda install -c conda-forge -c bioconda -c defaults abricate
 echo $?
+
+which abricate
+# expect /opt/conda/bin/abricate
+abricate --check
+abricate --list
+
+# def has ecoli_vf resfinder resfinder
+# so enough DB for now
 
 date
